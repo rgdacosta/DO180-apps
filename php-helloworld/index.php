41e0afdf2b040817f1207f72475a515f1fbe51c4
Student User <student@workstation.lab.example.com>
2020-04-22 16:50:49 +0200

    Change to Greeting

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello Advance Auto Parts' developers! php version is " . PHP_VERSION . "\n";
+print "Greeting Advance Auto Parts' developers! php version is " . PHP_VERSION . "\n";
 ?>
